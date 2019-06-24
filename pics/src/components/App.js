0886b78979ccbe9593d8b863994860d1e0ef7c0e
Chloe McDermott <chloemcdermott95@aol.com>
2019-06-24 20:44:46 +0100

    app set up - search engine API for images

---
+++ pics/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+
+class App extends React.Component {
+    onSearchSubmit(term){
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '20px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+            );
+        }
+    }
+  
+
+export default App; 
